d80351580914bf2975a4ba0fd27f16737ddb976d
leko <rexx0520@gmail.com>
2025-07-25 19:25:04 +0000

    Leko's config

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,10 +17,10 @@
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='R4AG_Router'' package/lean/default-settings/files/zzz-default-settings
 
 # 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-sed -i "s/OpenWrt /wbs build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-
-# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i "s/OpenWrt /lekoOwO build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改默认wifi名称ssid为Xiaomi_R4A
 sed -i 's/ssid=OpenWrt/ssid=Xiaomi_R4A/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修復 batman-adv 編譯錯誤
+cp "$GITHUB_WORKSPACE/compat-hacks.h" feeds/routing/batman-adv/src/
